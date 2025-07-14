47ba51eb7a3db556f62d7b3738b06597166de1e3
Erasmus Cedernaes <github@emaus.dev>
2025-07-14 20:30:14 +0200

    Display key codes on display

--- lcd.py
+++ lcd.py
@@ -9,10 +9,14 @@ I2C_NUM_ROWS = 2
 I2C_NUM_COLS = 16
 
 
-def test_main():
-    # Test function for verifying basic functionality
+def setup_lcd():
     i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)
-    lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
+    lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)    
+    lcd.show_cursor()
+    return lcd
+
+
+def test_main(lcd):
     lcd.putstr("It Works!")
     utime.sleep(2)
     lcd.clear()
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from lcd import test_main
+from lcd import setup_lcd
 from ps2_kb import scan_codes
 
-while True:
-    print("Starting LCD test...")
-    test_main()
+keys = []
+lcd = setup_lcd()
 
+while True:
     if scan_codes:
         code = scan_codes.pop()
         print("Scan code: {:02X}".format(code))
+        keys.append(code)
+        
+        lcd.clear()
+        
+        string = "".join([chr(a) for a in keys])
+        lcd.putstr(string)
