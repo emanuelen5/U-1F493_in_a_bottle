76e631ae1888da1cd5f0f815e98af2c536c1ced7
Erasmus Cedernaes <github@emaus.dev>
2025-08-21 17:09:18 +0200

    Make progress bar simpler

--- text_animator.py
+++ text_animator.py
@@ -3,7 +3,7 @@ from frame_updater import FrameUpdater
 from hd44780 import char_envelope_left, char_envelope_right
 
 # Must be at least the width of the display
-ship = "                .................. " + char_envelope_left + char_envelope_right
+ship = "." * 16 + char_envelope_left + char_envelope_right
 
 
 def sleep_gen(ms: float):
@@ -13,27 +13,25 @@ def sleep_gen(ms: float):
 
 
 def send_envelope_animator(frame: FrameUpdater, initial_speed_ms=300, final_speed_ms=50):
-    frame.lcd.hide_cursor()
+    frame.hide_cursor()
     frame.update_display_optimized("")
 
-    yield from sleep_gen(100)
-
-    total_steps = len(ship)
-    for pos in range(total_steps):
-        progress = min(max(0, pos / 10), 1)
+    total_steps = 18
+    for pos in range(1, total_steps):
+        progress = min(max(0, (pos - 1) / 10), 1)
         current_speed = initial_speed_ms - progress * (initial_speed_ms - final_speed_ms)
 
         visible_part = ship[-pos - 1:]
         visible_part = visible_part[:16]  # Ensure it fits the display width
 
-        frame.update_display_optimized(visible_part)
+        text = "Skickar         " + visible_part
+        frame.update_display_optimized(text[:32])
         yield from sleep_gen(int(current_speed))
 
-    yield from sleep_gen(300)
-    frame.update_display_optimized("    Sparat!")
+    frame.update_display_optimized("Klart!")
     yield from sleep_gen(1000)
     frame.update_display_optimized("")
-    frame.lcd.blink_cursor_on()
+    frame.show_cursor()
 
 
 if __name__ == "__main__":
@@ -41,7 +39,6 @@ if __name__ == "__main__":
     from lcd import setup_lcd
     from hd44780 import get_japanese_keycode_map, add_missing_characters
     lcd = setup_lcd()
-    lcd.blink_cursor_on()
 
     keycode_map = get_japanese_keycode_map()
     add_missing_characters(lcd, keycode_map)
